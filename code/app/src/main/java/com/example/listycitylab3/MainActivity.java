586f765b0ab2a051ac698881bc462772e33994f7
merci63 <mersimoyt27@gmail.com>
2025-09-19 15:02:55 -0600

    add edit functionality

--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,36 +1,63 @@
 package com.example.listycitylab3;
 
+
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener{
 
-    private ArrayList<String> dataList;
+    private ArrayList<City> dataList;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
+    private CityArrayAdapter cityAdapter;
 
+    int pos=-1;
+
+    public void addCity(City city){
+        if(pos == -1) {
+            cityAdapter.add(city);
+        }
+        cityAdapter.notifyDataSetChanged();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
+                "Edmonton", "Vancouver", "Toronto"
         };
+        String[] provinces = {"AB","BC","ON"};
 
-        dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
-        
+        dataList = new ArrayList<City>();
+        for(int i =0; i< cities.length; i++){
+            dataList.add(new City(cities[i],provinces[i]));
+        }
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        cityAdapter = new CityArrayAdapter(this, dataList);
         cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                pos = 1;
+                City selected_city = (City) cityList.getItemAtPosition(position);
+                new AddCityFragment(selected_city).show(getSupportFragmentManager(),"Edit City");
+            }
+        });
+
+        fab.setOnClickListener(v ->{
+            pos = -1;
+            new AddCityFragment().show(getSupportFragmentManager(),"Add City");
+        });
     }
 }
